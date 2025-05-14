20f432f8b6a7ee11aa1e6b8c487a286ae589e41f
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-05-14 15:04:38 +0700

    add manual loading in page using suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { BlogpostCard } from "@/components/general/BlogpostCard";
 import { prisma } from "./utils/db";
+import { Suspense } from "react";
 
 async function getData() {
   const data = await prisma.blogPost.findMany({
@@ -8,24 +10,36 @@ async function getData() {
       imageUrl: true,
       authorImage: true,
       authorName: true,
-      id:true,
+      id: true,
       createdAt: true,
+      authorId: true,
+      updatedAt: true,
     },
-  })
+  });
 
   return data;
 }
 
-export default async function Home() {
-  const data = await getData();
+export default function Home() {
   return (
     <div className="py-6">
       <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Post</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {data.map((item) => (
-          <h1 key={item.title}>{ item.title}</h1>
-        ))}
-      </div>
+
+      <Suspense fallback={<p>Hello waiting...</p>}>
+        <BlogPosts />
+      </Suspense>
+    </div>
+  );
+}
+
+async function BlogPosts() {
+  const data = await getData();
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {data.map((item) => (
+        <BlogpostCard data={item} key={item.id} />
+      ))}
     </div>
   );
 }
