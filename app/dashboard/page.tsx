4499a222321c38b818894703c9b65627970a43c3
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-05-15 15:18:46 +0700

    added user condition

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,6 +21,12 @@ export default async function DashboardRoute() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
+  if (!user) {
+    <div className="text-xl font-medium">
+      You must be logged in to view your dashboard
+    </div>;
+  }
+
   const data = await getData(user?.id);
 
   return (
